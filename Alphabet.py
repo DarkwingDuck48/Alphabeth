7e46402d253bcb60f2d0edadaec897db4bdaedca
Max Britvin <maksbritvin@gmail.com>
2016-07-28 12:38:19 +0400

    add Belorussian

--- Alphabet.py
+++ Alphabet.py
@@ -11,10 +11,11 @@ uninumber(char) - возвращает номер Unicode выбранной б�
 """
 # Основной класс (__init__ возможно использовать для формирования собственного алфавита)
 class Alphabet:
-    def __init__(self, name, chars, chartype):
+    def __init__(self, name, chars, chartype, codenumbers):
         self.name = name
         self.chars = chars
         self.chartype = chartype
+        self.codenumbers = codenumbers
 
     def upperсhars(self):
         if self.chartype == "Letters":
@@ -51,63 +52,72 @@ class Avestan(Alphabet):
         self.special_symbols = [chr(char) for char in range(68409, 68415)]
 
 
+class Belorussian(Alphabet):
+    def __init__(self):
+        self.name = "Belorussian"
+        self.chartype = "Letters"
+        self.codenumbers = [i for i in range(1040, 1046)]+[1025]+[i for i in range(1046,1048)]+[1030]+\
+                           [i for i in range(1049,1060)]+[1038]+[i for i in range(1060,1065)]+\
+                           [i for i in range(1067,1072)]+[i for i in range(1072,1078)]+[1105]+\
+                           [i for i in range(1078,1080)]+[1110]+[i for i in range(1081,1092)]+[1118]+\
+                           [i for i in range(1092,1097)]+[i for i in range(1099,1104)]
+        self.chars = [chr(char) for char in self.codenumbers]
 class Russian(Alphabet):
     def __init__(self):
         self.name = 'Russian'
         self.chartype = "Letters"
-        codenumbers = [i for i in range(1040, 1047)] + [1025] + [i for i in range(1047, 1078)] + [1105] + \
+        self.codenumbers = [i for i in range(1040, 1047)] + [1025] + [i for i in range(1047, 1078)] + [1105] + \
                       [i for i in range(1078, 1104)]
-        self.chars = [chr(char) for char in codenumbers]
+        self.chars = [chr(char) for char in self.codenumbers]
 
 
 class English(Alphabet):
     def __init__(self):
         self.name = "English"
-        self.chars = [chr(char) for char in range(65,123) if 'a' <= chr(char) <= 'z' or 'A' <= chr(char) <= 'Z']
         self.chartype = "Letters"
-
+        self.chars = [chr(char) for char in range(65,123) if 'a' <= chr(char) <= 'z' or 'A' <= chr(char) <= 'Z']
 
 class German(Alphabet):
     def __init__(self):
         self.name = "German"
-        codenumbers = [65, 196, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 214, 80, 81, 82, 83, 7838, 84,
-                       85, 220, 86, 87, 88, 89, 90, 97, 228, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109,
-                       110, 111, 246, 112, 113, 114, 115, 223, 116, 117, 252, 118, 119, 120, 121, 122]
-        self.chars = [chr(char) for char in codenumbers]
+        self.codenumbers = [65, 196, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 214, 80, 81, 82, 83, 7838,
+                            84, 85, 220, 86, 87, 88, 89, 90, 97, 228, 98, 99, 100, 101, 102, 103, 104, 105, 106,107,108,
+                            109, 110, 111, 246, 112, 113, 114, 115, 223, 116, 117, 252, 118, 119, 120, 121, 122]
+        self.chars = [chr(char) for char in self.codenumbers]
         self.chartype = "Letters"
 
 
 class Spanish(Alphabet):
     def __init__(self):
         self.name = "Spanish"
-        self.chartype = "Letter"
-        codenumbers = [i for i in range(65, 79)]+[209]+[i for i in range(79, 91)]+[i for i in range(97, 111)]+[241] + \
-            [i for i in range(111,123)]
-        self.chars = [chr(char) for char in codenumbers]
+        self.chartype = "Letters"
+        self.codenumbers = [i for i in range(65, 79)]+[209]+[i for i in range(79, 91)]+[i for i in range(97,111)]+[241]+\
+                           [i for i in range(111,123)]
+        self.chars = [chr(char) for char in self.codenumbers]
 
 
 class Italian(Alphabet):
     def __init__(self):
         self.name = "Italian"
-        self.chartype = "Letter"
-        codenumbers = [i for i in range(65, 74)]+[i for i in range(76, 87)]+[i for i in range(97, 106)] + \
+        self.chartype = "Letters"
+        self.codenumbers = [i for i in range(65, 74)]+[i for i in range(76, 87)]+[i for i in range(97, 106)] + \
                       [i for i in range(108, 119)]+[224, 232, 233, 236, 237, 238, 242, 243, 249, 250]
-        self.chars = [chr(char) for char in codenumbers]
+        self.chars = [chr(char) for char in self.codenumbers]
 
 
 class Bulgarian(Alphabet):
     def __init__(self):
         self.name = "Bulgarian"
-        self.chartype = "Letter"
-        codenumbers = [i for i in range(1040, 1067)]+[1068, 1070, 1071]+[i for i in range(1072, 1099)]+ \
+        self.chartype = "Letters"
+        self.codenumbers = [i for i in range(1040, 1067)]+[1068, 1070, 1071]+[i for i in range(1072, 1099)]+ \
                       [1100, 1102, 1103]
-        self.chars = [chr(char) for char in codenumbers]
+        self.chars = [chr(char) for char in self.codenumbers]
 
 
 class Greek(Alphabet):
     def __init__(self):
         self.name = "Greek"
-        self.chartype = "Greek letter"
-        codenumbers = [i for i in range(913, 938)]+[i for i in range(945, 970)]
-        self.chars = [chr(char) for char in codenumbers]
+        self.chartype = "Greek letters"
+        self.codenumbers = [i for i in range(913, 938)]+[i for i in range(945, 970)]
+        self.chars = [chr(char) for char in self.codenumbers]
 
